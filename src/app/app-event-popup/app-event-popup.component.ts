a562cae014a115601573a9be5d86abb41ba53d64
lokeshkaushik <lokeshrameshsharma10@gmail.com>
2024-09-10 21:11:00 +0530

    minor bug fixes

--- src/app/app-event-popup/app-event-popup.component.ts
+++ src/app/app-event-popup/app-event-popup.component.ts
@@ -6,21 +6,9 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app-event-popup.component.css']
 })
 export class AppEventPopupComponent {
-  isVisible: boolean = false; // Initialize the popup visibility
+  isVisible: boolean = true; // Initialize the popup visibility
 
-  @HostListener('window:scroll', [])
-  onWindowScroll(): void { // Return type must be 'void'
-    const sliderElement = document.querySelector('app-home-slider'); // Ensure selector is valid
-    const sliderHeight = sliderElement ? sliderElement.getBoundingClientRect().bottom : 0;
-
-    // Check if the scroll passed the bottom of the slider component
-    if (window.pageYOffset > sliderHeight) {
-      this.isVisible = true; // Show popup
-    } else {
-      this.isVisible = false; // Hide popup if scrolled back up
-    }
-  }
-
+  
   closePopup(): void {
     this.isVisible = false; // Close popup on button click
   }
